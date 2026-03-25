c136203274157eb89604a713ad1ebac3110ea49b
beto <capb1981@gmail.com>
2026-03-25 14:22:40 -0300

    aula 25/03/2026 ficou para casa exercício calcular média do trimestre

---
+++ src/main/java/aula/meuprojeto/aula/Aluno.java
@@ -0,0 +1,17 @@
+package aula.meuprojeto.aula;
+
+public class Aluno {
+
+  static String nome = "Eyshila";
+  static int idade = 17;
+  static boolean eAluno = true;
+  static double nota = 8.5;
+
+  public static void main(String[] args) {
+    System.out.println("Nome do aluno(a): " + nome);
+    System.out.println("Idade: " + idade);
+    System.out.println("É aluno(a): " + eAluno);
+    System.out.println("Nota: " + nota);
+  }
+
+}
---
+++ src/main/java/aula/meuprojeto/aula/Calculadora.java
@@ -0,0 +1,28 @@
+package aula.meuprojeto.aula;
+
+public class Calculadora {
+
+  public static void somar(double a, double b) {
+    System.out.println(a + b);
+  }
+
+  public static void subtrair(double a, double b) {
+    System.out.println(a - b);
+  }
+
+  public static void multiplicar(double a, double b) {
+    System.out.println(a * b);
+  }
+
+  public static void dividir(double a, double b) {
+    System.out.println(a / b);
+  }
+
+  public static void main(String[] args) {
+    // somar(1, 6);
+    // subtrair(-12, 6);
+    // multiplicar(70, 2.6);
+    dividir(15, 30);
+  }
+
+}
